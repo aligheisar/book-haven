f518b66c280bce6272f75e68b0e2724ca312267c
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-29 10:35:45 +0330

    moving auth logic to auth context

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -7,13 +7,18 @@ import {
 } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { getSession } from "../supabase/session";
+import { login, register } from "../supabase/auth";
+import { formatError } from "../util/format";
 import Loading from "../Components/Loading";
+import { GetNotifi } from "./NotifiContext";
 
 const authContext = createContext();
 
 export const GetAuth = () => useContext(authContext);
 
 export default function AuthProvider({ children }) {
+  let { addNotif } = GetNotifi();
+
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -39,7 +44,29 @@ export default function AuthProvider({ children }) {
     initializeUser();
   }, [initializeUser]);
 
-  const value = { user, setLoading };
+  let loginUser = async (email, password) => {
+    try {
+      await login(email, password);
+    } catch (error) {
+      addNotif({
+        ...formatError(error),
+        type: "danger",
+      });
+    }
+  };
+
+  let registerUser = async (email, password, fullName, username) => {
+    try {
+      await register(email, password, fullName, username);
+    } catch (error) {
+      addNotif({
+        ...formatError(error),
+        type: "danger",
+      });
+    }
+  };
+
+  const value = { user, setLoading, loginUser, registerUser };
 
   if (loading) return <Loading />;
 
