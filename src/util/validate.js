bdce5a657d24ee41ce8e5381163fa7c311a4a60f
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-29 15:03:51 +0330

    extendign validate func

--- src/util/validate.js
+++ src/util/validate.js
@@ -1,18 +1,24 @@
 import { emailSchema } from "../config/schema";
 
 export let validateInputs = (name, value) => {
+  if (value.length === 0) return null;
   if (name === "email") {
-    if (value.length > 0) {
-      let response = emailSchema.safeParse(value);
-      if (!response.success) {
-        return "Email is Not Valid";
-      }
-    }
-  } else if (name === "password") {
-    if (value.length > 0) {
-      if (value.length < 6) {
-        return "Password must be at least 6 char";
-      }
+    let response = emailSchema.safeParse(value);
+    if (!response.success) {
+      return "Email is Not Valid";
     }
   }
+  if (name === "password") {
+    if (value.length < 6) {
+      return "Password must be at least 6 char";
+    }
+  }
+  if (name === "username") {
+    if (value.includes(" ")) return "Username can't contains space";
+    if (value.length < 4) return "Username must be at least 4 char";
+  }
+  if (name === "fullName") {
+    if (value.trim().length < 3) return "full Name must be at least 3 char";
+  }
+  return null;
 };
