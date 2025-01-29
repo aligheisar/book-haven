7f1b7103a0be532b341edc75a449e5a3e5d49fe3
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-29 10:35:59 +0330

    changing supabase funcs

--- src/supabase/auth.js
+++ src/supabase/auth.js
@@ -1,29 +1,45 @@
 import { supabase } from "./client";
 
-export async function signup(email, password, fullName, username) {
-  try {
-    const { data: authData, error: authError } = await supabase.auth.signUp({
+async function getUserById(id) {
+  const { data, error } = await supabase.from("users").select("*").eq("id", id);
+
+  if (error) throw error;
+
+  return data;
+}
+
+async function insertUser(id, fullName, username) {
+  const { error } = await supabase.from("users").insert({
+    id,
+    username,
+    full_name: fullName,
+  });
+
+  if (error) throw error;
+}
+
+export async function register(email, password, fullName, username) {
+  const { data: authData, error: authError } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (authError) throw authError;
+
+  const userId = authData.user.id;
+
+  insertUser(userId, fullName, username);
+
+  let { avatar_url } = getUserById(userId);
+
+  return {
+    user: {
+      fullName,
       email,
-      password,
-    });
-
-    if (authError) throw new Error(`Auth Error: ${authError.message}`);
-
-    const userId = authData.user.id;
-
-    const { error: dbError } = await supabase.from("users").insert({
-      id: userId,
       username,
-      full_name: fullName,
-    });
-
-    if (dbError) throw new Error(`Database Error: ${dbError.message}`);
-
-    return { success: true };
-  } catch (error) {
-    console.error("Signup Error:", error.message);
-    return { success: false, message: error.message };
-  }
+      avatarUrl: avatar_url,
+    },
+  };
 }
 
 export async function login(email, password) {
@@ -33,7 +49,19 @@ export async function login(email, password) {
   });
 
   if (error) throw error;
-  return data;
+
+  let { userId } = data.user.id;
+
+  let { avatar_url, full_name, username } = getUserById(userId);
+
+  return {
+    user: {
+      fullName: full_name,
+      email,
+      username,
+      avatarUrl: avatar_url,
+    },
+  };
 }
 
 export async function logout() {
--- src/supabase/session.js
+++ src/supabase/session.js
@@ -1,15 +1,25 @@
 import { supabase } from "./client";
 
 export async function getSession() {
-  const { data, error } = await supabase.auth.getSession();
-  if (error) throw error;
-  if (data && data.session) {
+  const { data: sessionData, error: sessionError } =
+    await supabase.auth.getSession();
+
+  if (sessionError) throw sessionError;
+
+  if (sessionData && sessionData.session) {
     let { data: userData, error: userError } = await supabase
       .from("users")
       .select("*")
-      .eq("id", data.session.user.id);
+      .eq("id", sessionData.session.user.id);
+
     if (userError) throw userError;
-    return userData;
+
+    return {
+      fullName: userData.full_name,
+      email: sessionData.session.user.email,
+      username: userData.username,
+      avatarUrl: userData.avatar_url,
+    };
   }
   return null;
 }
