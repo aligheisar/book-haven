7b5244ee069b05ede1b3411fee833eff36eabfab
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-30 19:50:35 +0330

    adding more constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,4 @@
 export const SUPABASE_URL = "your supabase url";
+export const AVATAR_IMAGES = `${SUPABASE_URL}/storage/v1/object/public/avatar-images/`;
+export const BOOKS_IMAGES = `${SUPABASE_URL}/storage/v1/object/public/books-images/`;
 export const SUPABASE_KEY = "your supabase key";
