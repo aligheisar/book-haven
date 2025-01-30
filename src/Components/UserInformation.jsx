590f1763d6f0eba0e9c749d8cae37e805d269ac9
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-30 10:49:58 +0330

    useing editable text

--- src/Components/UserInformation.jsx
+++ src/Components/UserInformation.jsx
@@ -1,16 +1,23 @@
 import { GetAuth } from "../Context/AuthContext";
 import Avatar from "./Avatar";
+import EditableText from "./EditableText";
 
 let UserInformation = () => {
   let { user } = GetAuth();
   return (
     <section className="flex flex-col items-center">
-      <Avatar url={user.avatarUrl} className="z-10" name={user.fullName} />
-      <section className="-mt-4 flex w-full flex-col items-center bg-secondary-surface px-3 py-2 pt-6">
-        <h2 className="text-xl text-text">{user.fullName}</h2>
+      <Avatar url={user.avatarUrl} name={user.fullName} className="z-[1]" />
+      <section className="-mt-6 flex w-full flex-col items-center rounded-md bg-secondary-surface px-3 py-2 pt-8">
+        <EditableText
+          changeHandler={async (data) =>
+            new Promise((resolve, reject) =>
+              setTimeout(() => resolve(data), 2000),
+            )
+          }
+          content={user.fullName}
+        />
       </section>
     </section>
   );
 };
-
 export default UserInformation;
