8d554b8c42990df08235f19bbbd98fc5e9b773b6
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-30 19:49:39 +0330

    change format errro func

--- src/util/format.js
+++ src/util/format.js
@@ -1,3 +1,16 @@
 export let formatError = (error) => {
-  return { title: error?.status, desc: error.code?.split("_").join(" ") };
+  let title = "Faild";
+  let desc = "somting happends";
+  if (error.status) {
+    title = error.status;
+  } else if (error.statusCode) {
+    title = error.statusCode;
+  }
+  if (error.code) {
+    desc = error.code;
+  } else if (error.message) {
+    desc = error.message;
+  }
+
+  return { title, desc };
 };
