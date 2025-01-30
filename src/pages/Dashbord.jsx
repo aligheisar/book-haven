7025d7e5d2f0cbfa7b3195aebec89780f90a7bd6
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-30 09:04:15 +0330

    changing dashbord

--- src/pages/Dashbord.jsx
+++ src/pages/Dashbord.jsx
@@ -1,5 +1,21 @@
+import DashbordSection from "../Components/DashbordSection";
+import UserInformation from "../Components/UserInformation";
+import { GetAuth } from "../Context/AuthContext";
+
 let Dashbord = () => {
-  return <section>dashbord</section>;
+  let { user } = GetAuth();
+
+  return (
+    <section className="flex h-full w-full flex-col items-center gap-2 pt-6">
+      <DashbordSection>
+        <h1 className="font-serif text-2xl text-secondary-text">
+          Account Information
+        </h1>
+        <UserInformation />
+      </DashbordSection>
+      <DashbordSection>SOME OTHER INFO</DashbordSection>
+    </section>
+  );
 };
 
 export default Dashbord;
