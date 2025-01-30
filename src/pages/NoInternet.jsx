f6d1b4bf8c2138687da54b62902150af9cfbe31c
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-30 16:14:22 +0330

    adding no internet page

---
+++ src/pages/NoInternet.jsx
@@ -0,0 +1,12 @@
+let NoInternet = () => {
+  return (
+    <main className="grid h-full w-full -translate-y-12 place-content-center gap-4 text-center">
+      <h1 className="font-serif text-5xl text-primary">No Internet</h1>
+      <p className="text-lg text-secondary-text opacity-80">
+        please check your internet Connection
+      </p>
+    </main>
+  );
+};
+
+export default NoInternet;
