22a22ccfea9a12bde7760b4019a02aaf35b25b35
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-30 09:03:49 +0330

    changing the main layout

--- src/pages/layout/MainLayout.jsx
+++ src/pages/layout/MainLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
 
 let MainLayout = () => {
   return (
-    <>
+    <div className="flex h-full flex-col">
       <Navbar />
-      <main className="h-full overflow-y-auto bg-background">
+      <main className="flex-1 overflow-y-auto bg-background">
         <Outlet />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 };
 
