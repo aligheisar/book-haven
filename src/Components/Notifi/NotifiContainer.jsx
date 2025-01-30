ad1a6fd13939a1ea1335f820e2f3fe2541fe2396
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-01-30 10:48:12 +0330

    adding z index to notifications

--- src/Components/Notifi/NotifiContainer.jsx
+++ src/Components/Notifi/NotifiContainer.jsx
@@ -7,7 +7,7 @@ let NotifiContainer = () => {
 
   if (!notifications || notifications.length === 0) return null;
   return createPortal(
-    <section className="fixed left-1/2 top-16 flex w-72 -translate-x-1/2 flex-col gap-2">
+    <section className="fixed left-1/2 top-16 z-50 flex w-72 -translate-x-1/2 flex-col gap-2">
       {notifications.map((i) => (
         <NotifiItem key={i.id} notif={i} />
       ))}
