3359d05e3166ef6ad6576e435b5d85cfca1a85ff
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-02 19:46:01 +0330

    add new icon

--- src/Components/ui/Icons.jsx
+++ src/Components/ui/Icons.jsx
@@ -63,3 +63,14 @@ export let Loading = ({ size = 20, color = "black", inher, ...props }) => {
     </svg>
   );
 };
+
+export let Add = ({ size = 20, color = "black", inher, ...props }) => {
+  return (
+    <svg viewBox="0 0 24 24" width={size} height={size} {...props}>
+      <path
+        d="M20,11h-7V4c0-0.552-0.448-1-1-1s-1,0.448-1,1v7H4c-0.552,0-1,0.448-1,1s0.448,1,1,1h7v7c0,0.552,0.448,1,1,1s1-0.448,1-1 v-7h7c0.552,0,1-0.448,1-1S20.552,11,20,11z"
+        fill={!inher ? color : null}
+      />
+    </svg>
+  );
+};
