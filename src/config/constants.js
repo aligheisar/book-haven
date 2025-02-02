86eb6408ce6262765a95625ddcb0e8725e02ff55
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-02 15:46:53 +0330

    fix: wrong url env var name

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const AVATAR_IMAGES = `${process.env.SUPABASE_URL}/storage/v1/object/public/avatar-images/`;
-export const BOOKS_IMAGES = `${process.env.SUPABASE_URL}/storage/v1/object/public/books-images/`;
+export const AVATAR_IMAGES = `${process.env.REACT_APP_SUPABASE_URL}/storage/v1/object/public/avatar-images/`;
+export const BOOKS_IMAGES = `${process.env.REACT_APP_SUPABASE_URL}/storage/v1/object/public/books-images/`;
