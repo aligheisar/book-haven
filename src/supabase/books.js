95c3e93fa1118b89988f29d7344783e5c8d8059c
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-02 20:41:33 +0330

    adding some books backend logic

--- src/supabase/books.js
+++ src/supabase/books.js
@@ -27,14 +27,32 @@ export async function getBookDetails(bookId) {
   return data;
 }
 
-export async function addBook(userId, title, description, imageUrl) {
-  const { data, error } = await supabase.from("books").insert({
-    user_id: userId,
-    title,
-    description,
-    image_url: imageUrl,
+export async function addBook(
+  userId,
+  title,
+  description,
+  price,
+  imageUrl,
+  genres,
+) {
+  const { data, error } = await supabase.rpc("add_book_with_genres", {
+    p_user_id: userId,
+    p_title: title,
+    p_description: description,
+    p_price: price,
+    p_image_url: imageUrl || null,
+    p_genre_names: genres,
   });
 
   if (error) throw error;
-  return data;
+
+  return { success: true, data, error: null };
+}
+
+export async function getGenres() {
+  const { data, error } = await supabase.from("genres").select("name");
+
+  if (error) throw error;
+
+  return { success: true, data: data.map((i) => i.name), error: null };
 }
