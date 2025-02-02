a087125e5dc532bbf7cd612e0f47b438c4138a29
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-02 15:47:26 +0330

    separate editable text component

--- src/Components/EditableText.jsx
+++ src/Components/EditableText.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { cn } from "../util/cn.ts";
-import Input from "./ui/Input.tsx";
-import InputError from "./InputError.jsx";
-import { Done, Edit, Loading } from "./ui/Icons";
+import { Edit } from "./ui/Icons";
+import EditableTextInput from "./EditableTextInput";
 import useKeybordShortcuts from "../hooks/use-keybord-shortcuts";
 
 let EditableText = ({
@@ -57,33 +56,13 @@ let EditableText = ({
   });
 
   return editMode ? (
-    <div className="flex w-full flex-col gap-2">
-      <div className="relative h-[45px] w-full">
-        <Input
-          varient="filled"
-          autoFocus
-          disabled={loading}
-          onChange={handleInputChange}
-          value={inputValue.value}
-          onKeyDown={(e) => (e.keyCode === 13 ? done(true) : null)}
-          onBlur={() => done(true)}
-          className="bg-surface/30"
-          inputClass="text-center text-xl"
-          {...props}
-        />
-        <span
-          onClick={() => done(true)}
-          className="absolute right-2 top-1/2 -translate-y-1/2 rounded bg-text/0 fill-text/70 p-1 hover:bg-text/10"
-        >
-          {loading ? (
-            <Loading inher size="18" className="animate-spin" />
-          ) : (
-            <Done inher size="16" />
-          )}
-        </span>
-      </div>
-      {inputValue.error && <InputError>{inputValue.error}</InputError>}
-    </div>
+    <EditableTextInput
+      loading={loading}
+      handleInputChange={handleInputChange}
+      inputValue={inputValue}
+      props={props}
+      done={done}
+    />
   ) : (
     <h2
       className={cn([
