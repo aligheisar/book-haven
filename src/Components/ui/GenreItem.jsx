a3453c01477417fcfdafba627675029dc2aecf3d
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-06 19:48:42 +0330

    genre item no-wrap

--- src/Components/ui/GenreItem.jsx
+++ src/Components/ui/GenreItem.jsx
@@ -2,7 +2,7 @@ let GenreItem = ({ children, ...props }) => {
   return (
     <div
       {...props}
-      className="relative flex items-center gap-1 rounded-full border border-text/20 bg-secondary/60 px-2 text-text after:absolute after:inset-0 after:flex after:justify-center after:rounded-full after:bg-danger/40 after:opacity-0 after:backdrop-blur-md after:transition-opacity after:content-['X'] hover:border-danger hover:bg-danger/30 hover:after:opacity-100"
+      className="relative flex items-center text-nowrap rounded-full border border-text/20 bg-secondary/60 px-2 text-text transition-colors after:absolute after:inset-0 after:flex after:justify-center after:rounded-full after:bg-danger/40 after:opacity-0 after:backdrop-blur-md after:transition-opacity after:content-['X'] hover:border-danger hover:bg-danger/30 hover:after:opacity-100"
     >
       {children}
     </div>
