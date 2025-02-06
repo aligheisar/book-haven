7a4b168c97cc918959fbf5fe4ae981e6bf50cfa3
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-06 19:48:56 +0330

    text center on book image placholder

--- src/Components/BookImagePlaceholder.jsx
+++ src/Components/BookImagePlaceholder.jsx
@@ -1,7 +1,7 @@
 let BookImagePlaceholder = () => {
   return (
     <div className="flex h-full w-full items-center justify-center bg-background text-lg">
-      <h2 className="text-secondary">No Book Image</h2>
+      <h2 className="text-center text-secondary">No Book Image</h2>
     </div>
   );
 };
