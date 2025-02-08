7c74df3c9f7862ef20261a94b2a99c3e812ba0a8
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-08 08:23:33 +0330

    start designing book details page

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -1,23 +1,39 @@
 import { useParams } from "react-router-dom";
-import { getBookDetails } from "../supabase/books";
-import { useEffect, useState } from "react";
+import Loading from "../Components/Loading";
+import useBookDetails from "../hooks/use-book-details";
+import BookImage from "../Components/BookImage";
 
 let BookDetails = () => {
-  let params = useParams();
+  let { username, book } = useParams();
 
-  const [data, setData] = useState({});
+  let [data, error, loading] = useBookDetails(username, book);
 
-  useEffect(() => {
-    let fetchData = async () => {
-      let response = await getBookDetails(params.username, params.title);
-      setData(response);
-    };
+  if (loading) return <Loading />;
+  if (error) return <h1>we got a error</h1>;
 
-    fetchData();
-  }, [params]);
-
-  console.log(data);
-  return <h1>book</h1>;
+  return (
+    <section className="flex flex-col">
+      <header className="flex h-96 gap-7 py-7">
+        <BookImage url={data.imageUrl} />
+        <div className="flex flex-col gap-6 py-3">
+          <div className="flex flex-col gap-3">
+            <h1 className="font-serif text-5xl text-primary">{data.title}</h1>
+            <p className="custom-scroll-light max-h-16 overflow-y-auto text-text opacity-80">
+              {data.description || "no description"}
+            </p>
+          </div>
+          <div className="flex flex-col gap-3">
+            <section>author data</section>
+            <section>likes and comments</section>
+          </div>
+        </div>
+      </header>
+      <section>
+        <div>add comment</div>
+        <section>comments</section>
+      </section>
+    </section>
+  );
 };
 
 export default BookDetails;
