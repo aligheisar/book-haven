ae5e81a6bb47de4584731ead8ae5aec64067baa2
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-08 19:48:53 +0330

    book not found component

---
+++ src/pages/BookNotFound.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import Button from "../Components/ui/Button.tsx";
+
+let BookNotFound = () => {
+  return (
+    <main className="flex h-full w-full -translate-y-12 flex-col items-center justify-center gap-8 text-center">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-serif text-6xl text-primary">Not Found</h1>
+        <p className="text-lg text-secondary-text opacity-80">
+          we can't find this book
+        </p>
+      </div>
+      <Link to="/">
+        <Button varient="dim" className="text-base">
+          Back to Home
+        </Button>
+      </Link>
+    </main>
+  );
+};
+
+export default BookNotFound;
