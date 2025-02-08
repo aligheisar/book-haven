2b3422d465a34f4be2185ed77e010fe5d67d2460
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-08 19:45:39 +0330

    adding custom scroll to description

--- src/Components/ui/Description.jsx
+++ src/Components/ui/Description.jsx
@@ -4,12 +4,12 @@ let Description = ({ className, children, ...props }) => {
   return (
     <p
       className={cn(
-        "custom-scroll-light max-h-16 overflow-y-auto text-sm text-text opacity-70",
+        `custom-scroll-light max-h-16 overflow-y-auto text-sm text-text ${!children ? "line-through opacity-40" : "opacity-70"}`,
         className,
       )}
       {...props}
     >
-      {children}
+      {children || "No description"}
     </p>
   );
 };
