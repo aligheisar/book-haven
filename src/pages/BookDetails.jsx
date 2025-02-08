f7d7e274ae0254e4ba9e4f213db3985fa224372e
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-08 19:49:38 +0330

    changing book details

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -5,6 +5,9 @@ import BookImage from "../Components/BookImage";
 import Title from "../Components/ui/Title";
 import Description from "../Components/ui/Description";
 import UserInfo from "../Components/UserInfo";
+import Price from "../Components/ui/Price";
+import BookState from "../Components/BookState";
+import BookNotFound from "./BookNotFound";
 
 let BookDetails = () => {
   let { username, book } = useParams();
@@ -12,20 +15,26 @@ let BookDetails = () => {
   let [data, error, loading] = useBookDetails(username, book);
 
   if (loading) return <Loading />;
-  if (error) return <h1>we got a error</h1>;
+  if (error) return <BookNotFound />;
 
   return (
     <section className="flex flex-col">
       <header className="mx-auto flex h-96 w-fit gap-7 py-7">
         <BookImage url={data.imageUrl} />
-        <div className="flex max-w-72 flex-col gap-5 py-3">
+        <div className="flex max-w-80 flex-col gap-5 py-3">
           <div className="flex flex-col gap-3">
             <Title>{data.title}</Title>
-            <Description>{data.description || "No description"}</Description>
+            <Description>{data.description}</Description>
+            <Price>{data.price}</Price>
           </div>
           <div className="flex flex-col gap-3">
             <UserInfo username={username} />
-            <section>likes and comments</section>
+            <BookState
+              comments={data.comments}
+              likes={data.likes}
+              username={username}
+              book={book}
+            />
           </div>
         </div>
       </header>
