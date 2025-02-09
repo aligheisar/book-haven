70db3880a920c01e50de08a4290da8886ad4bb18
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-09 20:49:36 +0330

    changin book state logic

--- src/Components/BookState.jsx
+++ src/Components/BookState.jsx
@@ -1,33 +1,18 @@
-import { GetUser } from "../Context/UserContext";
-import useOptimistic from "../hooks/use-optimistic";
-import { toggleLike, isLikedByUsername } from "../supabase/likes";
 import LikeSection from "./LikeSection";
 import CommentCount from "./CommentCount";
-import { useCallback } from "react";
+import { GetBookDetails } from "../Context/BookDetailsContext";
 
-let BookState = ({ likes, comments, username, book }) => {
-  let { user } = GetUser();
-
-  let checkIsLiked = useCallback(async () => {
-    return await isLikedByUsername(user.username, book);
-  }, [book, user.username]);
-
-  let handler = async () => {
-    let { result } = await toggleLike(user.username, username, book);
-    return result;
-  };
-
-  let [isLiked, likeAction, loading] = useOptimistic(checkIsLiked, handler);
+let BookState = () => {
+  let { toggleLike, data } = GetBookDetails();
 
   return (
     <section className="flex gap-2">
       <LikeSection
-        likes={likes}
-        isLiked={isLiked}
-        username={username}
-        toggleLike={() => likeAction((prev) => !prev)}
+        likes={data.likes}
+        isLiked={data.isUserLiked}
+        toggleLike={toggleLike}
       />
-      <CommentCount comments={comments.length} />
+      <CommentCount comments={data.comments.length} />
     </section>
   );
 };
