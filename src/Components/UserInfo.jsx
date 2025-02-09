1cabfdd04c132587ff8987e3875679eeda95f5df
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-09 20:49:55 +0330

    changing userInfo logic

--- src/Components/UserInfo.jsx
+++ src/Components/UserInfo.jsx
@@ -1,48 +1,36 @@
 import { Link } from "react-router-dom";
-import useFollowUser from "../hooks/use-follow-user";
 import Button from "./ui/Button.tsx";
-import { Loading } from "./ui/Icons";
+import { GetBookDetails } from "../Context/BookDetailsContext";
+import { GetUser } from "../Context/UserContext";
 
-let UserInfo = ({ username }) => {
-  let [targetUser, user, loading, subLoading, { toggleFollow }] =
-    useFollowUser(username);
+let UserInfo = () => {
+  let { user } = GetUser();
+
+  let { data, toggleFollow } = GetBookDetails();
 
   return (
     <section className="flex h-10 w-fit items-center justify-center gap-3 fill-primary text-text">
-      {loading ? (
-        <div className="flex items-center gap-2 text-secondary-text">
-          <Loading className="animate-spin" inher />
-          loading
-        </div>
-      ) : (
-        <>
-          <Link to={`/users/${username}`}>
-            <div className="flex items-center gap-2 rounded-full bg-surface p-1 pr-3">
-              <img
-                src={targetUser.avatarUrl}
-                className="size-7 rounded-full"
-                alt="user"
-              />
-              <h3 className="w-fit text-nowrap">{targetUser.fullName}</h3>
-            </div>
-          </Link>
-          {user.username !== username && (
-            <Button
-              onClick={toggleFollow}
-              disabled={subLoading}
-              varient="outlined"
-              className="relative h-fit rounded px-2 py-[2px] text-sm"
-            >
-              {subLoading && (
-                <span className="absolute inset-0 flex items-center justify-center bg-black/50">
-                  <Loading inher size={14} className="animate-spin" />
-                </span>
-              )}
-              {targetUser.isFollow ? "Following" : "Follow"}
-            </Button>
-          )}
-        </>
-      )}
+      <>
+        <Link to={`/users/${data.user.username}`}>
+          <div className="flex items-center gap-2 rounded-full bg-surface p-1 pr-3">
+            <img
+              src={data.user.avatarUrl}
+              className="size-7 rounded-full"
+              alt="user"
+            />
+            <h3 className="w-fit text-nowrap">{data.user.fullName}</h3>
+          </div>
+        </Link>
+        {user.username !== data.user.username && (
+          <Button
+            onClick={toggleFollow}
+            varient="outlined"
+            className="relative h-fit rounded px-2 py-[2px] text-sm"
+          >
+            {data.isUserFollow ? "Following" : "Follow"}
+          </Button>
+        )}
+      </>
     </section>
   );
 };
