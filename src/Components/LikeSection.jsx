756b905c6ad75c2b5e3b04bbb4ade35e4a8a7666
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-10 09:59:36 +0330

    change the padding and text size of like section

--- src/Components/LikeSection.jsx
+++ src/Components/LikeSection.jsx
@@ -4,9 +4,9 @@ let LikeSection = ({ isLiked, likes, toggleLike }) => {
   return (
     <div
       onClick={toggleLike}
-      className={`flex items-center gap-1 rounded-full px-1 py-1 pr-3 font-serif text-text transition ${isLiked ? "bg-secondary-surface fill-text" : "fill-secondary-text"}`}
+      className={`flex items-center gap-1 rounded-full px-1 py-[3px] pr-3 font-serif transition ${isLiked ? "bg-secondary-surface fill-text text-text" : "fill-secondary-text text-secondary-text"}`}
     >
-      <span className="flex size-6 items-center justify-center">
+      <span className="flex size-6 items-center justify-center text-sm">
         <Like inher size={24} />
       </span>
       {likes}
