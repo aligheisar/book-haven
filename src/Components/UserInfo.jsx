6555eff08c0463996c4e54e292acce069e1b4266
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-10 09:58:38 +0330

    changing the gap of userinfo and removing useless fragment

--- src/Components/UserInfo.jsx
+++ src/Components/UserInfo.jsx
@@ -9,28 +9,26 @@ let UserInfo = () => {
   let { data, toggleFollow } = GetBookDetails();
 
   return (
-    <section className="flex h-10 w-fit items-center justify-center gap-3 fill-primary text-text">
-      <>
-        <Link to={`/users/${data.user.username}`}>
-          <div className="flex items-center gap-2 rounded-full bg-surface p-1 pr-3">
-            <img
-              src={data.user.avatarUrl}
-              className="size-7 rounded-full"
-              alt="user"
-            />
-            <h3 className="w-fit text-nowrap">{data.user.fullName}</h3>
-          </div>
-        </Link>
-        {user.username !== data.user.username && (
-          <Button
-            onClick={toggleFollow}
-            varient="outlined"
-            className="relative h-fit rounded px-2 py-[2px] text-sm"
-          >
-            {data.isUserFollow ? "Following" : "Follow"}
-          </Button>
-        )}
-      </>
+    <section className="flex h-10 w-fit items-center justify-center gap-2 fill-primary text-text">
+      <Link to={`/users/${data.user.username}`}>
+        <div className="flex items-center gap-2 rounded-full bg-surface p-1 pr-3">
+          <img
+            src={data.user.avatarUrl}
+            className="size-7 rounded-full"
+            alt="user"
+          />
+          <h3 className="w-fit text-nowrap">{data.user.fullName}</h3>
+        </div>
+      </Link>
+      {user.username !== data.user.username && (
+        <Button
+          onClick={toggleFollow}
+          varient="outlined"
+          className="relative h-fit rounded px-2 py-[2px] text-sm"
+        >
+          {data.isUserFollow ? "Following" : "Follow"}
+        </Button>
+      )}
     </section>
   );
 };
