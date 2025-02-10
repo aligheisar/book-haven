eacb6704e4087bb5285b4f759769c119026719c3
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-10 19:52:39 +0330

    adding time formater

--- src/util/format.js
+++ src/util/format.js
@@ -14,3 +14,25 @@ export let formatError = (error) => {
 
   return { title, desc: desc.split("_").join(" ") };
 };
+
+export const timeAgo = (utcString) => {
+  const date = new Date(utcString);
+  const diffMs = Date.now() - date.getTime();
+  const diffSeconds = Math.floor(diffMs / 1000);
+  const diffMinutes = Math.floor(diffSeconds / 60);
+  const diffHours = Math.floor(diffMinutes / 60);
+  const diffDays = Math.floor(diffHours / 24);
+
+  if (diffSeconds < 60) return `${diffSeconds} seconds ago`;
+  if (diffMinutes < 60) return `${diffMinutes} minutes ago`;
+  if (diffHours < 24) return `${diffHours} hours ago`;
+  return `${diffDays} days ago`;
+};
+
+export const generateUTCTimestamp = () => {
+  const date = new Date();
+
+  const utcTimestamp = date.toISOString().replace("Z", "+00:00");
+
+  return utcTimestamp;
+};
