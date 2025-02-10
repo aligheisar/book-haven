dbb580874298ef66bf4776203dbecfff325c0c62
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-10 09:59:11 +0330

    make comment count number bigger

--- src/Components/CommentCount.jsx
+++ src/Components/CommentCount.jsx
@@ -2,8 +2,8 @@ import { Comment } from "./ui/Icons";
 
 let CommentCount = ({ comments }) => {
   return (
-    <div className="flex items-center gap-1 rounded-full fill-secondary-text px-1 font-serif text-text">
-      <span className="flex size-6 items-center justify-center">
+    <div className="flex items-center gap-1 rounded-full fill-secondary-text px-1 font-serif text-secondary-text">
+      <span className="flex size-6 items-center justify-center text-sm">
         <Comment inher size={20} />
       </span>
       {comments}
