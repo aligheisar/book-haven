dd00f170839e0dc5773d5dc7bc74f28635ece9f5
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-10 19:51:53 +0330

    using comment components in book details

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -3,6 +3,8 @@ import { GetBookDetails } from "../context/BookDetailsContext";
 import BookImage from "../components/BookImage";
 import BookNotFound from "./BookNotFound";
 import BookInformation from "../components/BookInformation";
+import AddComment from "../components/AddComment";
+import CommentSection from "../components/CommentSection";
 
 let BookDetails = () => {
   let { data, error, pageLoading } = GetBookDetails();
@@ -11,14 +13,14 @@ let BookDetails = () => {
   if (error) return <BookNotFound />;
 
   return (
-    <section className="flex flex-col">
+    <section className="mx-auto flex max-w-[720px] flex-col">
       <header className="mx-auto flex h-96 w-fit gap-6 py-7">
         <BookImage url={data.imageUrl} />
         <BookInformation data={data} />
       </header>
-      <section>
-        <div>add comment</div>
-        <section>comments</section>
+      <section className="flex flex-col gap-2">
+        <AddComment />
+        <CommentSection comments={data.comments} />
       </section>
     </section>
   );
