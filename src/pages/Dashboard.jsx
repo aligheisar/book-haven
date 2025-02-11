fa7ab9d4fa31b8dcd86c0315dd0a3c85fefde27e
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-11 13:27:50 +0330

    changing dashboard layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,25 +2,36 @@ import DashboardSection from "../components/DashboardSection";
 import AccountInformation from "../components/AccountInformation";
 import SectionTitle from "../components/SectionTitle";
 import Button from "../components/ui/Button.tsx";
-import { Add } from "../components/ui/Icons";
+import { Add, Refresh } from "../components/ui/Icons";
 import { Link } from "react-router-dom";
 import UserBooks from "../components/UserBooks";
+import { GetUser } from "../context/UserContext";
 
 let Dashboard = () => {
+  let { fetchUserBooks } = GetUser();
+
   return (
     <section className="flex h-full w-full items-start justify-center gap-4 pt-6">
-      <DashboardSection>
+      <DashboardSection className="max-w-[380px] shrink-0">
         <SectionTitle>Account Information</SectionTitle>
         <AccountInformation />
       </DashboardSection>
-      <DashboardSection className="max-w-none flex-1">
+      <DashboardSection className="flex-1">
         <div className="flex items-center justify-between">
           <SectionTitle className="text-start">Your Books</SectionTitle>
-          <Link to="/new-book">
-            <Button varient="outlined" Icon={<Add inher size="18" />}>
-              Add Book
-            </Button>
-          </Link>
+          <div className="flex items-center gap-2">
+            <span
+              onClick={fetchUserBooks}
+              className="rounded-sm fill-secondary-text p-2 hover:bg-text/5"
+            >
+              <Refresh size={16} inher />
+            </span>
+            <Link to="/new-book">
+              <Button varient="outlined" Icon={<Add inher size="18" />}>
+                Add Book
+              </Button>
+            </Link>
+          </div>
         </div>
         <UserBooks />
       </DashboardSection>
