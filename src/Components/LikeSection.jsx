651464c1da6f59eabcc0eb3b5b6dcf17bb65e3ee
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-11 17:22:23 +0330

    adding hover for like button

--- src/Components/LikeSection.jsx
+++ src/Components/LikeSection.jsx
@@ -4,7 +4,7 @@ let LikeSection = ({ isLiked, likes, toggleLike }) => {
   return (
     <div
       onClick={toggleLike}
-      className={`flex items-center gap-1 rounded-full px-1 py-[3px] pr-3 font-serif transition ${isLiked ? "bg-secondary-surface fill-text text-text" : "fill-secondary-text text-secondary-text"}`}
+      className={`flex items-center gap-1 rounded-full px-1 py-[3px] pr-3 font-serif transition ${isLiked ? "bg-secondary-surface fill-text text-text" : "fill-secondary-text text-secondary-text hover:bg-secondary-surface/20"}`}
     >
       <span className="flex size-6 items-center justify-center text-sm">
         <Like inher size={24} />
