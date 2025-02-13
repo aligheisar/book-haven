6aabc43174c4b1ab5e2c465215fb08841f2ebef9
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-13 16:26:50 +0330

    adding modal renderer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,13 @@
 import App from "./App";
 import NotifiContainer from "./components/notifi/NotifiContainer";
+import ModalRenderer from "./components/modal/ModalRenderer";
 import Providers from "./context/Providers";
 
 let Root = () => {
   return (
     <Providers>
       <App />
+      <ModalRenderer />
       <NotifiContainer />
     </Providers>
   );
