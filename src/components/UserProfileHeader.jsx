d2040e47e32395ecf0f9c1c8b7e3202b58c2e50e
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-13 19:26:14 +0330

    design user profile page

---
+++ src/components/UserProfileHeader.jsx
@@ -0,0 +1,46 @@
+import UserAvatar from "./UserAvatar";
+import FollowButton from "./ui/FollowButton";
+
+let UserProfileHeader = ({ data, user, toggleFollowUser }) => {
+  return (
+    <header className="flex items-center justify-between gap-5 py-7">
+      <div className="flex items-center gap-8">
+        <UserAvatar url={data.avatarUrl} />
+        <div className="flex flex-col gap-2">
+          <h2 className="text-center font-serif text-3xl text-text">
+            {data.fullName}
+          </h2>
+          <p className="mb-1 text-center text-primary">{data.username}</p>
+          {user.username !== data.username && (
+            <FollowButton
+              toggleFollow={toggleFollowUser}
+              isFollow={data.isUserFollow}
+            />
+          )}
+        </div>
+      </div>
+      <div className="flex gap-3">
+        <p className="flex flex-col-reverse items-center gap-1 rounded-md bg-surface p-2 text-text">
+          books
+          <span className="font-serif text-xl text-primary drop-shadow-md">
+            {data.books.length}
+          </span>
+        </p>
+        <p className="flex flex-col-reverse items-center gap-1 rounded-md bg-surface p-2 text-text">
+          followers
+          <span className="font-serif text-xl text-primary drop-shadow-md">
+            {data.followers}
+          </span>
+        </p>
+        <p className="flex flex-col-reverse items-center gap-1 rounded-md bg-surface p-2 text-text">
+          following
+          <span className="font-serif text-xl text-primary drop-shadow-md">
+            {data.following}
+          </span>
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default UserProfileHeader;
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,9 +1,125 @@
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { toggleFollow, userPagebyUsername } from "../supabase/user";
+import { GetUser } from "../context/UserContext";
+import { GetNotifi } from "../context/NotifiContext";
+import UserProfileHeader from "../components/UserProfileHeader";
+import PageContainer from "../components/PageContainer";
+import GridSection from "../components/GridSection";
+import BookCard from "../components/BookCard";
+import Loading from "../components/Loading";
+import UserNotFound from "./UserNotFound";
 
 let UserProfile = () => {
-  let { username } = useParams();
+  let { username: targetUsername } = useParams();
 
-  return <h1>{username}</h1>;
+  let { user } = GetUser();
+  let { addNotif } = GetNotifi();
+
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  let fetchUserData = useCallback(async () => {
+    try {
+      let { success, data } = await userPagebyUsername(targetUsername);
+      if (success && data) {
+        let {
+          username,
+          full_name,
+          avatar_url,
+          followers_count,
+          following_count,
+          is_following,
+          books,
+        } = data;
+        setUserData({
+          fullName: full_name,
+          username,
+          avatarUrl: avatar_url,
+          followers: followers_count,
+          following: following_count,
+          isUserFollow: is_following,
+          books: books.map((i) => ({
+            title: i.title,
+            imageUrl: i.image_url,
+            price: i.price,
+            user: {
+              username: i.user.username,
+              fullName: i.user.full_name,
+            },
+          })),
+        });
+      }
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }, [targetUsername]);
+
+  useEffect(() => {
+    if (!userData) {
+      fetchUserData();
+    }
+  }, [userData, fetchUserData]);
+
+  let toggleFollowUser = async () => {
+    if (!user) {
+      addNotif({
+        type: "warning",
+        title: "Your not Logged in",
+        desc: "Please first login to your account",
+      });
+      return;
+    }
+
+    let prevState = userData;
+
+    setUserData((prev) => ({
+      ...prev,
+      isUserFollow: !prev.isUserFollow,
+      followers: prev.isUserFollow ? prev.followers - 1 : prev.followers + 1,
+    }));
+
+    try {
+      await toggleFollow(targetUsername);
+    } catch (error) {
+      setUserData(prevState);
+    }
+  };
+
+  if (loading) return <Loading />;
+  if (error) return <UserNotFound />;
+
+  return (
+    <PageContainer>
+      <UserProfileHeader
+        toggleFollowUser={toggleFollowUser}
+        data={userData}
+        user={user}
+      />
+      <GridSection>
+        {userData.books && userData.books.length > 0 ? (
+          userData.books.map((i) => (
+            <BookCard
+              key={i.title}
+              fullName={i.user.fullName}
+              image={i.imageUrl}
+              price={i.price}
+              title={i.title}
+              username={i.user.username}
+              bg="bg-background"
+            />
+          ))
+        ) : (
+          <p className="w-full pt-6 text-center text-secondary-text/60">
+            No book available
+          </p>
+        )}
+      </GridSection>
+    </PageContainer>
+  );
 };
 
 export default UserProfile;
