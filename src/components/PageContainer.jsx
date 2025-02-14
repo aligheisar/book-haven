6f04f4ea1db6fb3dcb43646c7a6b34f4a7e3125b
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-02-14 18:04:28 +0330

    change max width on page container

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -3,7 +3,7 @@ import { cn } from "../util/cn.ts";
 let PageContainer = ({ children, className, ...props }) => {
   return (
     <section
-      className={cn("mx-auto flex max-w-[720px] flex-col", className)}
+      className={cn("mx-auto flex max-w-[790px] flex-col", className)}
       {...props}
     >
       {children}
